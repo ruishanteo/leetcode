6c9acc5c524571ea3bdaf3f635eeb3a5d02510f1
ruishanteo <ruishan.teo03@gmail.com>
2023-05-10 09:36:33 +0800

    3Sum

---
+++ 15. 3Sum/Solution.py
@@ -0,0 +1,30 @@
+class Solution(object):
+    def threeSum(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: List[List[int]]
+        """
+        answer = []
+        nums.sort()
+        for i in range(len(nums) - 2):
+            if i > 0 and nums[i] == nums[i-1]:
+                continue
+            j = i + 1
+            k = len(nums) - 1
+            while j < k:
+                s = nums[i] + nums[j] + nums[k]
+                if s == 0:
+                    triplet = [nums[i], nums[j], nums[k]]
+                    answer.append(triplet)
+                   
+                    while j < k and nums[j] == nums[j+1]:
+                        j += 1
+                    while j < k and nums[k] == nums[k-1]:
+                        k -= 1
+                    j += 1
+                    k -= 1
+                elif s < 0:
+                    j += 1
+                else:
+                    k -= 1
+        return answer
